abf56d07fe3447fa9c9abdfdd77b1ce1c50f4c53
おう <15621624+xbw@users.noreply.github.com>
2022-12-01 14:17:20 +0800

    PEP 8 Style

--- interfacelift-downloader.py
+++ interfacelift-downloader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from __future__ import print_function
+
 # Imports for Python 3 and 2 respectively
 try:
     from urllib.request import urlopen, Request
@@ -15,6 +16,7 @@ import threading
 import time
 import argparse
 
+
 # Merge dictionaries. Used for merging resolutions.
 # Necessary workaround for Python 2.x   :(
 def merge_dicts(*dict_args):
@@ -22,6 +24,7 @@ def merge_dicts(*dict_args):
     for dictionary in dict_args:
         result.update(dictionary)
     return result
+
 
 HOST = 'http://interfacelift.com'
 RES_WIDESCREEN_16_10 = {
@@ -125,11 +128,12 @@ RES_PATHS = merge_dicts(
 IMG_PATH_PATTERN = re.compile(r'<a href=\"(?P<path>.+)\"><img.+?src=\"/img_NEW/button_download')
 IMG_FILE_PATTERN = re.compile(r'[^/]*$')
 
+
 # Downloads the given url and write it to the given directory
 def download_file(url, saveDir):
     # interfacelift returns a 403 forbidden unless you include a referer.
-    headers = { 'User-Agent' : "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)",
-                'Referer': url}
+    headers = {'User-Agent': "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)",
+               'Referer': url}
     req = Request(url, None, headers)
     filename = IMG_FILE_PATTERN.search(url).group()
     saveFile = os.path.join(saveDir, filename)
@@ -140,8 +144,11 @@ def download_file(url, saveDir):
             print('[+] Downloaded %s' % filename)
         except Exception as e:
             print(e)
-            try: os.remove(saveFile)
-            except: pass
+            try:
+                os.remove(saveFile)
+            except:
+                pass
+
 
 # Thread worker. Constantly takes URLs from the queue
 def download_worker():
@@ -150,28 +157,33 @@ def download_worker():
         download_file(url, SAVE_DIR)
         queue.task_done()
 
+
 # Returns the path of the specified page number
 def get_page_path(pageNumber):
     return '%sindex%d.html' % (RES_PATH, pageNumber)
+
 
 # Returns the full URL of the specified path
 def get_url_from_path(path):
     return '%s/%s' % (HOST, path)
 
+
 # Returns the full URL of the specified page number
 def get_page_url(pageNumber):
     return get_url_from_path(get_page_path(pageNumber))
 
+
 # Returns True if next page exists, else False
 def has_next_page(pageContent, currentPage):
-    return True if pageContent.find(get_page_path(currentPage+ 1)) > -1 else False
+    return True if pageContent.find(get_page_path(currentPage + 1)) > -1 else False
+
 
 # Opens the specified page and returns the page's HTML content
 def open_page(pageNumber):
     url = get_page_url(pageNumber)
     # interfacelift returns a 403 forbidden unless you include a referer.
-    headers = { 'User-Agent' : "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)",
-                'Referer': url}
+    headers = {'User-Agent': "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)",
+               'Referer': url}
     try:
         req = Request(url, None, headers)
         f = urlopen(req)
@@ -181,11 +193,13 @@ def open_page(pageNumber):
             print('Error code:', e.code)
     return f.read().decode(errors='ignore')
 
+
 # Returns the specified number of seconds in H:MM:SS format
 def pretty_time(seconds):
     m, s = divmod(round(seconds), 60)
     h, m = divmod(m, 60)
     return "%d:%02d:%02d" % (h, m, s)
+
 
 # Prints the list of supported resolutions
 def print_resolution_list():
@@ -204,6 +218,7 @@ def print_resolution_list():
     print('\n[iPad resolutions]')
     print(', '.join(key for key in RES_IPAD))
 
+
 # Validates the supplied arguments
 def validate_args(parser, args):
     if args.list:
@@ -215,6 +230,7 @@ def validate_args(parser, args):
         print('List available resolutions: %s --list' % os.path.basename(__file__))
         sys.exit(1)
 
+
 # Prints the starting variables for the script
 def print_starting_vars():
     print('Selected resolution: %s' % args.resolution)
@@ -222,10 +238,12 @@ def print_starting_vars():
     print('Threads: %s' % THREADS)
     if OVERWRITE:
         print('Overwrite: enabled')
-    
+
+
 # Parse arguments
 parser = argparse.ArgumentParser(description='Download wallpapers from interfacelift.com')
-parser.add_argument('resolution', nargs='?', help='the resolution to download (default: 1920x1080)', default='1920x1080')
+parser.add_argument('resolution', nargs='?', help='the resolution to download (default: 1920x1080)',
+                    default='1920x1080')
 parser.add_argument('-d', '--dest', help='the directory to download to (default: ./wallpapers)', default='wallpapers')
 parser.add_argument('-t', '--threads', help='the number of threads to use (default: 4)', default=4, type=int)
 parser.add_argument('-o', '--overwrite', help='overwrite files with same name (default: disabled)', action='store_true')
@@ -276,6 +294,6 @@ while True:
     else:
         break
 
-queue.join() # block until all urls processed
+queue.join()  # block until all urls processed
 print('[*] Download finished! (%d files)' % count)
 print('[*] Time taken: %s' % pretty_time(time.time() - timeStart))
